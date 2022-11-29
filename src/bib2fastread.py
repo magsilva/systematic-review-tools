729384263fe140369393b35c9083dd6311b735d0
Marco Aurélio Graciotto Silva <magsilva@utfpr.edu.br>
2022-11-29 02:50:57 -0300

    Ignore entries with mandatory fields (#2) and for certain entry types (#3).

--- src/bib2fastread.py
+++ src/bib2fastread.py
@@ -15,14 +15,21 @@ class BibtexFilter(object):
         self.input_bibtex_file = None
         self.output_csv_file = None
         self.fields = dict()
+        self.mandatory_fields = set()
         self.default_field_value = dict()
+        self.entry_types = set()
 
-    def add_field(self, header_name, fields, default_value):
+    def add_entry_type(self, entry_type):
+        self.entry_types.add(entry_type)
+
+    def add_field(self, header_name, fields, default_value, is_mandatory = False):
         if not header_name in self.fields:
             self.fields[header_name] = []
             self.default_field_value[header_name] = default_value
         if fields is not None:
             self.fields[header_name] += fields
+        if is_mandatory:
+            self.mandatory_fields.add(header_name)
 
     def set_input_file(self, bibtex_filename):
         self.input_bibtex_file = bibtex_filename
@@ -31,9 +38,13 @@ class BibtexFilter(object):
         self.output_csv_file = csv_filename
 
     def run(self):
-        parser = BibTexParser(ignore_nonstandard_types = False, homogenize_fields = False, common_strings = True, customization = add_plaintext_fields)
+        parser = BibTexParser(common_strings = True, ignore_nonstandard_types = False, homogenize_fields = False, customization = add_plaintext_fields, add_missing_from_crossref = True)
         with open(self.input_bibtex_file) as bibtex_input_file:
-            raw_database = parser.parse_file(bibtex_input_file)
+            try:
+                raw_database = parser.parse_file(bibtex_input_file)
+            except UnicodeDecodeError:
+                with open(self.input_bibtex_file, encoding = "latin-1") as bibtex_input_file_fallback:
+                    raw_database = parser.parse_file(bibtex_input_file_fallback) 
         final_database = BibDatabase()
 
         with open(self.output_csv_file, 'w', newline='') as csvfile:
@@ -41,21 +52,31 @@ class BibtexFilter(object):
             csv_headers = self.fields.keys()
             csvwriter.writerow(csv_headers)
             for entry in raw_database.entries:
-                csv_row = []
-                for header, fields in self.fields.items():
-                    for field in fields:
-                        try:
-                            if entry[field]:
-                                if field == 'doi':
-                                    csv_row.append('https://dx.doi.org/' + entry[field])
+                if not self.entry_types or entry["ENTRYTYPE"] in self.entry_types:
+                    csv_row = []
+                    entry_status = True
+                    for header, fields in self.fields.items():
+                        field_status = False
+                        for field in fields:
+                            try:
+                                if entry[field]:
+                                    field_status = True
+                                    if field == 'doi':
+                                        csv_row.append('https://dx.doi.org/' + entry[field])
+                                    else:
+                                        csv_row.append(entry[field])
+                                    break
+                            except KeyError:
+                                pass
+                        else:
+                            if entry_status:
+                                if not field_status and header in self.mandatory_fields:
+                                    print("Error: insufficient data for entry: ", entry["ID"], entry["ENTRYTYPE"])
+                                    entry_status = False
                                 else:
-                                    csv_row.append(entry[field])
-                                break
-                        except KeyError:
-                            pass
-                    else:
-                        csv_row.append(self.default_field_value[header])
-                csvwriter.writerow(csv_row)
+                                    csv_row.append(self.default_field_value[header])
+                    if entry_status:                
+                         csvwriter.writerow(csv_row)
 
 if __name__ == "__main__":
     bibtex_filename = sys.argv[1]
@@ -64,7 +85,9 @@ if __name__ == "__main__":
     bibtex_filter = BibtexFilter()
     bibtex_filter.set_input_file(bibtex_filename)
     bibtex_filter.set_output_csv_file(filename + '.csv')
-    bibtex_filter.add_field('Document Title', ['title'], '')
+    bibtex_filter.add_entry_type('inproceedings')
+    bibtex_filter.add_entry_type('article')
+    bibtex_filter.add_field('Document Title', ['title'], '', True)
     bibtex_filter.add_field('Abstract', ['abstract'], '')
     bibtex_filter.add_field('Year', ['year'], 0)
     bibtex_filter.add_field('PDF Link',['doi', 'url'], '')
